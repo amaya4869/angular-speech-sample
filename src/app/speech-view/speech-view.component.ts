d4cf69719318290701ebf1c0c5d09f70e4997b9a
Sasaki Yuuma <yuma4869@gmail.com>
2019-09-29 17:16:15 +0900

    fix: 認識した音声を表示

--- src/app/speech-view/speech-view.component.ts
+++ src/app/speech-view/speech-view.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { SpeechGeneratorService } from '../speech-generator.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-speech-view',
   templateUrl: './speech-view.component.html',
   styleUrls: ['./speech-view.component.scss']
 })
-export class SpeechViewComponent implements OnInit {
+export class SpeechViewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public speechList: string[] = [];
+  public stateText = 'Stopping';
+
+  private subject: Subscription;
+
+  constructor(
+    @Inject(SpeechGeneratorService) private speechRecognitionService = new SpeechGeneratorService()
+  ) { }
 
   ngOnInit() {
+    this.subject = this.speechRecognitionService.onSpeechRecognition.subscribe((str: string) => {
+      this.speechList.push(str);
+      console.log(this.speechList);
+    });
   }
 
+  ngOnDestroy() {
+    this.subject.unsubscribe();
+  }
+
+  onClickStartButton() {
+    this.speechRecognitionService.start();
+    this.stateText = 'Starting';
+  }
+
+  onClickStopButton() {
+    this.speechRecognitionService.stop();
+    this.stateText = 'Stopping';
+  }
 }
